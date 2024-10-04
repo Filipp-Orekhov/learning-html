136f95826b10772c5d06a1fb35b591da5595e26a
Filipp Orekhov <orekhov_splav@bk.ru>
2024-10-04 23:36:19 +0300

    js homework 1

---
+++ Js/Test.js
@@ -0,0 +1,39 @@
+
+// const n = 10;
+// let s = 0;
+// for (let i = 0; i <= n; i++){
+// s = s + i;
+// }
+// console.log(s) /*сумма
+
+// const n = 10;
+// let fact = 1;
+// for (let i = 1; i <= n; i++){
+//     fact = fact * i
+// }
+// console.log(fact) /*факториал
+
+// for (let i = 1; i % 2 == 0 && i <= 100; i ++){
+// console.log(i)
+// }/* не врубился, что не так
+
+// for (let i=2; i <= 100; i += 2){
+//     console.log(i)   
+// } /*чёт
+
+// for (let i=1; i <= 100; i += 2){
+//     console.log(i)   
+// } /*нечёт
+ 
+// let p = 8;
+// let s = 0;
+// for (let i = 1; i <= p; i++) {
+//     s += i ** 2;
+// }
+// console.log(s); /*сумма квадратов
+
+// const p = 7;
+// let res = 0;
+// for(i = 0; i <= 10; i++){
+//     res = p * i, console.log(res)
+// }
